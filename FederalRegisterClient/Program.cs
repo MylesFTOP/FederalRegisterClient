562a49f079af0ed6922dd16b3796386aad6f92fb
MylesFTOP <m.fullen87@gmail.com>
2021-02-07 22:47:34 +0000

    Initial implementation - doesn't yet write response to DocumentModel (ReadAsAsync appears to be deprecated)

--- FederalRegisterClient/Program.cs
+++ FederalRegisterClient/Program.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace FederalRegisterClient
@@ -9,6 +11,20 @@ namespace FederalRegisterClient
     class Program
     {
         static HttpClient client = Factory.CreateHttpClient();
+
+        static void ShowDocument(DocumentModel document) {
+            Console.WriteLine($"Document Reference: {document.FederalRegisterDocumentNumber}");
+        }
+
+        static async Task<DocumentModel> GetDocumentAsync(DocumentModel document) {
+            HttpResponseMessage response = await client
+                .GetAsync($"{document.FederalRegisterDocumentNumber}.json");
+            // string responseBody = null;
+            // if (response.IsSuccessStatusCode) {
+            //     responseBody = await response.Content.ReadAsStringAsync();
+            // }
+            return document;
+        }
 
         static void Main() {
             RunAsync().GetAwaiter().GetResult();
@@ -18,9 +34,15 @@ namespace FederalRegisterClient
             client.BaseAddress = new Uri("https://www.federalregister.gov/api/v1/documents/");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
 
-            try { }
+            try {
+                DocumentModel document = Factory.CreateDocument();
+                document.FederalRegisterDocumentNumber = "01-27917"; // EO 13233, "Further Implementation of the Presidential Records Act"
+                document = await GetDocumentAsync(document);
+                ShowDocument(document);
+            }
             catch(Exception e) {
                 Console.WriteLine(e.Message);
             }
