f38a0e044bc8970b417b63148367157b49618267
MylesFTOP <m.fullen87@gmail.com>
2021-02-07 19:03:08 +0000

    Top-level call of async task

--- FederalRegisterClient/Program.cs
+++ FederalRegisterClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace FederalRegisterClient
 {
@@ -10,6 +11,19 @@ namespace FederalRegisterClient
         static HttpClient client = Factory.CreateHttpClient();
 
         static void Main() {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync() {
+            client.BaseAddress = new Uri("https://www.federalregister.gov/api/v1/documents/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try { }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
