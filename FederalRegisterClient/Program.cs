88ce5297b23cf425e38a6f6202ac5be05d720689
MylesFTOP <m.fullen87@gmail.com>
2021-02-07 23:25:32 +0000

    Move remaining to DocumentHandler to keep related items together

--- FederalRegisterClient/Program.cs
+++ FederalRegisterClient/Program.cs
@@ -1,48 +1,11 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace FederalRegisterClient
 {
     class Program
     {
-        static HttpClient client = Factory.CreateHttpClient();
-
-        static async Task<DocumentModel> GetDocumentAsync(DocumentModel document) {
-            HttpResponseMessage response = await client
-                .GetAsync($"{document.FederalRegisterDocumentNumber}.json");
-            string responseBody = null;
-            if (response.IsSuccessStatusCode) {
-                responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Response body: {responseBody}");
-            }
-            return document;
-        }
-
         static void Main() {
-            RunAsync().GetAwaiter().GetResult();
-        }
-
-        static async Task RunAsync() {
-            client.BaseAddress = new Uri("https://www.federalregister.gov/api/v1/documents/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json")
-                );
-
-            try {
-                DocumentModel document = Factory.CreateDocument();
-                document.FederalRegisterDocumentNumber = "01-27917"; // EO 13233, "Further Implementation of the Presidential Records Act"
-                document = await GetDocumentAsync(document);
-                DocumentHandler.ShowDocument(document);
-            }
-            catch(Exception e) {
-                Console.WriteLine(e.Message);
-            }
-        }
+            DocumentHandler.RunAsync().GetAwaiter().GetResult();
+        }        
     }
 }
--- FederalRegisterClient/Services/DocumentHandler.cs
+++ FederalRegisterClient/Services/DocumentHandler.cs
@@ -1,13 +1,48 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FederalRegisterClient
 {
     public static class DocumentHandler
     {
+        public static HttpClient client = Factory.CreateHttpClient();
+
+        public static async Task<DocumentModel> GetDocumentAsync(DocumentModel document) {
+            HttpResponseMessage response = await client
+                .GetAsync($"{document.FederalRegisterDocumentNumber}.json");
+            string responseBody = null;
+            if (response.IsSuccessStatusCode) {
+                responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Response body: {responseBody}");
+            }
+            return document;
+        }
+
         public static void ShowDocument(DocumentModel document) {
             Console.WriteLine($"Document Reference: {document.FederalRegisterDocumentNumber}");
+        }
+
+        public static async Task RunAsync() {
+            client.BaseAddress = new Uri("https://www.federalregister.gov/api/v1/documents/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+            try {
+                DocumentModel document = Factory.CreateDocument();
+                document.FederalRegisterDocumentNumber = "01-27917"; // EO 13233, "Further Implementation of the Presidential Records Act"
+                document = await GetDocumentAsync(document);
+                ShowDocument(document);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
