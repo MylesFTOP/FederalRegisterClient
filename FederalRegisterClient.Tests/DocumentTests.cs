3602ee190ecc740daa3ccf1982345a86bcb9b703
MylesFTOP <m.fullen87@gmail.com>
2021-02-08 23:51:52 +0000

    Second test for type expectation - currently failing due to duplicate client configuration

--- FederalRegisterClient.Tests/DocumentTests.cs
+++ FederalRegisterClient.Tests/DocumentTests.cs
@@ -11,6 +11,13 @@ namespace FederalRegisterClient.Tests
         [Fact]
         public async Task DocumentHandler_GetDocumentAsync_ShouldRetrieveDocument() {
             DocumentHandler.ConfigureClient();
+            var document = await DocumentHandler.GetDocumentAsync("01-27917");
+            Assert.IsType<DocumentModel>(document);
+        }
+
+        [Fact]
+        public async Task DocumentHandler_GetDocumentAsync_ShouldRetrieveExpectedDocument() {
+            DocumentHandler.ConfigureClient();
             var expected = "01-27917"; // EO 13233, "Further Implementation of the Presidential Records Act"
             var document = await DocumentHandler.GetDocumentAsync(expected);
             var actual = document.FederalRegisterDocumentNumber;
