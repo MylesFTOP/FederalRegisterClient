1a40c0bc28dc1d2886c24b2e98185b3774c29b34
MylesFTOP <m.fullen87@gmail.com>
2021-02-08 23:19:17 +0000

    Initial test before refactoring test code out of core application (currently fails as client is not configured)

---
+++ FederalRegisterClient.Tests/DocumentTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FederalRegisterClient.Tests
+{
+    public class DocumentTests
+    {
+        [Fact]
+        public async Task DocumentHandler_GetDocumentAsync_ShouldRetrieveDocument() {
+            var expected = "01-27917"; // EO 13233, "Further Implementation of the Presidential Records Act"
+            var document = await DocumentHandler.GetDocumentAsync(expected);
+            var actual = document.FederalRegisterDocumentNumber;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
