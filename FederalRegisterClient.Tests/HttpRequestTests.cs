070b201fa551a297ac5cb9516f98e6742b018b3a
MylesFTOP <m.fullen87@gmail.com>
2021-10-23 23:39:12 +0100

    New retry test - intended to confirm that eventual success in retry will return document

--- FederalRegisterClient.Tests/HttpRequestTests.cs
+++ FederalRegisterClient.Tests/HttpRequestTests.cs
@@ -17,7 +17,7 @@ namespace FederalRegisterClient.Tests
         [InlineData(HttpStatusCode.ServiceUnavailable, 3)]
         [InlineData(HttpStatusCode.TooManyRequests, 3)]
         [InlineData(HttpStatusCode.NotFound, 1)]
-        public async Task GetDocumentAsJsonAsync_ShouldRetryIfStatusCodeIsRetryable(HttpStatusCode httpStatusCode, int numberOfTries)
+        public async Task GetDocumentAsJsonAsync_ShouldRetryExpectedNumberOfTimesIfStatusCodeIsRetryable(HttpStatusCode httpStatusCode, int numberOfTries)
         {
             var httpRequestHandler = new HttpRequestHandler();
             var handlerMock = new Mock<HttpMessageHandler>();
@@ -50,8 +50,47 @@ namespace FederalRegisterClient.Tests
                 );
         }
 
-        [Fact(Skip = "Test has external dependency")]
-        public async Task DocumentHandler_ShouldReturnDocument()
+        [Theory]
+        [InlineData(HttpStatusCode.ServiceUnavailable)]
+        [InlineData(HttpStatusCode.TooManyRequests)]
+        public async Task GetDocumentAsJsonAsync_ShouldReturnDocumentIfRetryIsSuccessful(HttpStatusCode httpStatusCode)
+        {
+            var httpRequestHandler = new HttpRequestHandler();
+            var handlerMock = new Mock<HttpMessageHandler>();
+            TimeSpan timeSpanDelay = default;
+            var retryCondition = new RetryConditionHeaderValue(timeSpanDelay).ToString();
+            var httpResponseMessage = new HttpResponseMessage
+            {
+                StatusCode = httpStatusCode,
+                Content = httpStatusCode == HttpStatusCode.OK 
+                    ? new StringContent(@"{ ""document_number"" : ""01-27917""}", Encoding.UTF8, "application/json") 
+                    : new StringContent("")
+            };
+            httpResponseMessage.Headers.Add("Retry-After", retryCondition);
+
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponseMessage);
+
+            var httpClient = new HttpClient(handlerMock.Object);
+            HttpRequestHandler.ConfigureClient(httpClient, "https://www.mock.test/");
+
+            var expected = "01-27917"; // EO 13233, "Further Implementation of the Presidential Records Act"
+            var document = await DocumentHandler.GetDocumentAsync(expected);
+            Assert.NotNull(document);
+            handlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(3),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task DocumentHandler_ShouldReturnADocument()
         {
             var httpRequestHandler = new HttpRequestHandler();
             var handlerMock = new Mock<HttpMessageHandler>();
@@ -70,7 +109,7 @@ namespace FederalRegisterClient.Tests
                 .ReturnsAsync(httpResponseMessage);
 
             var httpClient = new HttpClient(handlerMock.Object);
-            HttpRequestHandler.ConfigureClient(httpClient, "https://www.federalregister.gov/api/v1/documents/");
+            HttpRequestHandler.ConfigureClient(httpClient, "https://www.mock.test/");
 
             var expected = "01-27917"; // EO 13233, "Further Implementation of the Presidential Records Act"
             var document = await DocumentHandler.GetDocumentAsync(expected);
