8a73c585aa00d004b36264f59c880e093884147f
MylesFTOP <m.fullen87@gmail.com>
2021-11-06 22:03:22 +0000

    Separate project for console UI logic

---
+++ FederalRegisterClient.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FederalRegisterClient.Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            HttpRequestHandler.ConfigureClient(
+                Factory.CreateHttpClient(), "https://www.federalregister.gov/api/v1/documents/");
+            DocumentHandler
+                .RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
--- FederalRegisterClient/Program.cs
+++ FederalRegisterClient/Program.cs
@@ -5,10 +5,7 @@ namespace FederalRegisterClient
     class Program
     {
         static void Main() {
-            HttpRequestHandler.ConfigureClient(
-                Factory.CreateHttpClient(), "https://www.federalregister.gov/api/v1/documents/");
-            DocumentHandler
-                .RunAsync().GetAwaiter().GetResult();
+            
         }        
     }
 }
