5d4a046922e5bfea398907c0784a68f33385b7fa
MylesFTOP <m.fullen87@gmail.com>
2021-11-06 22:53:05 +0000

    Move remaining display logic to console project; simplify references following namespace update

--- FederalRegisterClient.ConsoleApp/ConsoleUI.cs
+++ FederalRegisterClient.ConsoleApp/ConsoleUI.cs
@@ -8,18 +8,44 @@ namespace FederalRegisterClient.ConsoleApp
 {
     public static class ConsoleUI
     {
+        private static void ShowDocuments(List<DocumentModel> documents)
+        {
+            documents.ForEach(x => ShowDocument(x));
+        }
+
+        public static void ShowDocument(DocumentModel document)
+        {
+            Dictionary<string, string> documentDisplay = new Dictionary<string, string> {
+                { "Document Reference", document.FederalRegisterDocumentNumber },
+                { "Document Title", document.DocumentTitle },
+                { "Publication Date", document.PublicationDate.ToShortDateString() },
+                { "Publication Type", document.PublicationType },
+                { "Presidential Document Type", document.PresidentialDocumentType },
+                { "Document citation", document.DocumentCitation },
+                { "Abstract", document.Abstract },
+            };
+
+            foreach (KeyValuePair<string, string> item in documentDisplay)
+            {
+                if (!String.IsNullOrEmpty(item.Value))
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+
         public static async Task RunAsync()
         {
-            System.Console.WriteLine("Please enter document reference to retrieve:");
-            String documentNumberToRetrieve = System.Console.ReadLine();
+            Console.WriteLine("Please enter document reference to retrieve:");
+            String documentNumberToRetrieve = Console.ReadLine();
             try
             {
                 var document = await DocumentHandler.GetDocumentAsync(documentNumberToRetrieve);
-                DocumentHandler.ShowDocument(document);
+                ShowDocument(document);
             }
             catch (Exception e)
             {
-                System.Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message);
             }
         }
     }
--- FederalRegisterClient/Services/DocumentHandler.cs
+++ FederalRegisterClient/Services/DocumentHandler.cs
@@ -21,31 +21,5 @@ namespace FederalRegisterClient
         {
             return await HttpRequestHandler.GetDocumentAsJsonAsync(documentNumber);
         }
-
-        private static void ShowDocuments(List<DocumentModel> documents)
-        {
-            documents.ForEach(x => ShowDocument(x));
-        }
-
-        public static void ShowDocument(DocumentModel document)
-        {
-            Dictionary<string, string> documentDisplay = new Dictionary<string, string> {
-                { "Document Reference", document.FederalRegisterDocumentNumber },
-                { "Document Title", document.DocumentTitle },
-                { "Publication Date", document.PublicationDate.ToShortDateString() },
-                { "Publication Type", document.PublicationType },
-                { "Presidential Document Type", document.PresidentialDocumentType },
-                { "Document citation", document.DocumentCitation },
-                { "Abstract", document.Abstract },
-            };
-
-            foreach (KeyValuePair<string, string> item in documentDisplay)
-            {
-                if (!String.IsNullOrEmpty(item.Value))
-                {
-                    Console.WriteLine($"{item.Key}: {item.Value}");
-                }
-            }
-        }
     }
 }
